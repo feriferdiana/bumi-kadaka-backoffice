af17822ddc056a9bed8e3502159412d27f76c9f1
Feri <feri.ferdiana@adiwisista.com>
2025-02-08 21:22:39 +0700

    api email

---
+++ action/email_action.php
@@ -0,0 +1,21 @@
+<?php
+
+function insert($firstName, $lastName, $email, $subject, $message){
+	global $conn;
+
+	date_default_timezone_set('Asia/Jakarta');
+
+	$name = $firstName . " " . $lastName;
+
+	$id = uniqid();
+	$dateNow = date('Y-m-d H:i:s');
+
+	$sql = "INSERT INTO email (id, name, email, subject, message, created_date) VALUES (?,?,?,?,?,?)";
+	$stmt = $conn->prepare($sql);
+	$stmt->bind_param("ssssss", $id, $name, $email, $subject, $message, $dateNow);
+	$stmt->execute();
+	$stmt->close();
+	$conn->close();
+}
+
+?>
---
+++ restfull/email.php
@@ -0,0 +1,46 @@
+<?php
+    include "../action/con/connection.php";
+    include "../action/email_action.php";
+
+    header("Content-Type: application/json");
+    header("Access-Control-Allow-Origin: *");
+
+    $rawData = file_get_contents("php://input");
+
+    $data = json_decode($rawData, true);
+
+    if (json_last_error() === JSON_ERROR_NONE) {
+
+        // request
+        $fisrtName = $data['fisrtName'];
+        $lastName = $data['lastName'];
+        $email = $data['email'];
+        $subject = $data['subject'];
+        $message = $data['message'];
+
+        insert($fisrtName, $lastName, $email, $subject, $message);
+
+        // Respon
+        echo json_encode([
+            'code' => '200',
+            'status' => 'success',
+            'message' => 'Data received successfully',
+            'data' => [
+                'fisrtName' => $fisrtName,
+                'lastName' => $lastName,
+                'email' => $email,
+                'subject' => $subject,
+                'message' => $message
+            ]
+        ]);
+        
+
+    } else {
+        // Jika data JSON tidak valid
+        echo json_encode([
+            'code' => '500',
+            'status' => 'error',
+            'message' => 'Invalid JSON payload',
+        ]);
+    }
+?>
